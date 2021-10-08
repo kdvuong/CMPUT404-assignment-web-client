4e1fd93e45e3f1e791f65d029df444dfdd482fd0
Khang Vuong <khangdieuvuong@gmail.com>
2021-10-07 22:26:40 -0600

    add license

--- httpclient.py
+++ httpclient.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
+# Copyright 2021 Khang Vuong
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
